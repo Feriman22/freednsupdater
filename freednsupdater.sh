c62afffa65cff9c46454ea9e1e41a266ab3a4306
Feriman <Feriman22@users.noreply.github.com>
2023-10-23 20:38:50 +0200

    Update freednsupdater.sh

--- freednsupdater.sh
+++ freednsupdater.sh
@@ -4,23 +4,30 @@ echo "This script will update your DDNS address at afraid.org to your current IP
 Hosted on GitHub: https://github.com/Feriman22/freednsupdater
 Written by Feriman (Feriman.com)
 
-All logs are stored in /tmp/externalip.txt file inside the container.
+Set these variables in your docker-compose.yml:
+APIURL # Required. Go to https://freedns.afraid.org/api/, login and click on the "ASCII" link. Add the URL to your browser as a variable
+DDNSDOMAIN # Optional. If you have multiple domains, define them, otherwise the script will exit.
+CheckAgainInXSec # Optional. If you set a valid number, the external IP will be checked for changes every $CheckAgainInXSec seconds. The minimum value is 10.
+
+Debug:
+All logs are stored in the /tmp/afraid-ddns-ip-updater.log file inside the container.
 
 Exit codes:
 70: APIURL variable is missing
-71: You have multiple DDNS addresses, and not defined the DDNSDOMAIN variable
+71: You have multiple DDNS addresses, and have not defined the DDNSDOMAIN variable
 72: curl command not found (not installed or not placed in default paths)
-73: Do not flood anybody. You set the sleep time too low. It's a built in feature to avoid overload afraid.org or any IP check provider.
+73: Do not flood anyone. You have set the CheckAgainInXSec value too low. It's a built-in feature to avoid overloading afraid.org or any IP check provider.
+
 "
 
 # Variables
 [ ! "$APIURL" ] && echo "$(date) - ERROR: APIURL variable is missing. Exit." | tee "$LOG" && exit 70
-[[ "$REPEATSEC" != ?(-)+([0-9]) ]] && REPEATSEC=300  # Set default if not exists or invalid
+[[ "$CheckAgainInXSec" != ?(-)+([0-9]) ]] && CheckAgainInXSec=300  # Set default if not exists or invalid
 IPSTORE="/tmp/externalip.txt"
 LOG="/tmp/afraid-ddns-ip-updater.log"
 
 # Generate variables
-[ "$(curl -s "$APIURL" | wc -l)" -gt "1" ] && [ ! "$DDNSDOMAIN" ] && echo "$(date) - ERROR: You have multiple DDNS addresses, and not defined the DDNSDOMAIN variable. Exit." | tee "$LOG" && exit 71
+[ "$(curl -s "$APIURL" | wc -l)" -gt "1" ] && [ ! "$DDNSDOMAIN" ] && echo "$(date) - ERROR: You have multiple DDNS addresses, and have not defined the DDNSDOMAIN variable. Exit." | tee "$LOG" && exit 71
 [ ! "$DDNSDOMAIN" ] && DDNSDOMAIN="$(curl -s "$APIURL" | awk -F'|' '{print $1;}')"
 UPDATEURL="$(curl -s "$APIURL" | awk -F'|' '{print $3;}')"
 
@@ -28,7 +35,7 @@ UPDATEURL="$(curl -s "$APIURL" | awk -F'|' '{print $3;}')"
 ! command -v curl > /dev/null && echo "$(date) - ERROR: curl command not found. Exit." | tee "$LOG" && exit 72
 
 # Avoid flood
-[ "$REPEATSEC" -lt "10" ] && echo "$(date) - ERROR: Do not flood anybody. Exit." | tee "$LOG" && exit 73
+[ "$CheckAgainInXSec" -lt "10" ] && echo "$(date) - ERROR: Do not flood anyone. You have set the CheckAgainInXSec value too low. Exit." | tee "$LOG" && exit 73
 
 # Valiadte IP function
 validateip() {
@@ -86,8 +93,8 @@ while true; do
 		echo "$(date) - WARNING: We don't have a valid IP. Current value of IP is $CURRENT_IP" | tee "$LOG"
 	fi
 
-	# Sleep for $REPEATSEC seconds
-	sleep "$REPEATSEC"
+	# Sleep for $CheckAgainInXSec seconds
+	sleep "$CheckAgainInXSec"
 
 	# Validate that the DDNS IP has been updated
 	if [ "$IPUPDATED" ]; then
@@ -97,7 +104,7 @@ while true; do
 			echo "$CURRENT_IP" > "$IPSTORE"
 			unset IPUPDATED DDNSIP
 		else
-			echo "$(date) - WARNING: IP update failed this time (too low REPEATSEC value?). Maybe next time." | tee "$LOG"
+			echo "$(date) - WARNING: IP update failed this time (too low CheckAgainInXSec value?). Maybe next time." | tee "$LOG"
 			unset DDNSIP
 		fi
 	fi
